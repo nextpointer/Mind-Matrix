0ac821e873f7e46f6cbb5bb81266c87fadd489ea
lazy34 <maitysurajit0901@gmail.com>
2024-11-24 16:32:56 +0530

    Some fixes in UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Button } from "../Components/Button"
 import { MainHeading } from "../Components/MainHeading"
 import { SubHeading } from "../Components/SubHeading"
 import { Link } from "react-router-dom"
+import NormalButtons from "../Components/NormalButton"
 
 export const Home = () => {
   return (
@@ -12,7 +13,7 @@ export const Home = () => {
                 <p>MIND MATRIX</p>
             </div>
             <div className="homepage-login-section">
-                <Button >Alraedy a User?</Button>
+                <span>Already a User</span><Link to={"/user/login"}><NormalButtons text="Login" /></Link>
             </div>
         </div>
         <div id="homepage-hero-section">
@@ -26,9 +27,6 @@ export const Home = () => {
             <div className="hero-getting-start">
               <Link to={'/user/login'} className="linkdashboard"><Button/></Link>
             </div>
-          </div>
-          <div className="hero-illustration">
-              <img src="/images/hero.png" alt="gfggfg" />
           </div>
         </div>
     </>
