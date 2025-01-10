3ab87689b6ebf8e020e258aa4179c69228d1573a
lazy34 <maitysurajit0901@gmail.com>
2025-01-10 16:04:24 +0530

    some-fixing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "*", 
+    origin: "http://localhost:5173", 
     credentials: true // allow session cookie from browser to pass through
   })
 );
