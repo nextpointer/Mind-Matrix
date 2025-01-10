51377ec5fca475adb7f32d71de6655e28e7683f3
lazy34 <maitysurajit0901@gmail.com>
2025-01-10 06:10:47 +0530

    fixed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,7 +7,7 @@ import { app } from "./app.js";
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 3000, () => {
       console.log(`port is running on ${process.env.PORT}`);
     });
   })
