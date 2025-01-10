38c0b5ee47c0bd54e656b8834038f41bd413c2c1
lazy34 <maitysurajit0901@gmail.com>
2025-01-10 15:41:22 +0530

    fix: update CORS origin to allow all domains

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "https://mindrix.vercel.app/", 
+    origin: "*", 
     credentials: true // allow session cookie from browser to pass through
   })
 );
