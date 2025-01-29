a0fb417381b3d4b8c24d85f758f2e3eb8ca92775
lazy34 <maitysurajit0901@gmail.com>
2025-01-30 00:12:14 +0530

    fix: add partitioned option to cookie settings for enhanced security

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -92,6 +92,7 @@ export const loginUser = asyncHandler(async (req, res) => {
   const option = {
     secure: true,
     sameSite: "none",
+    partitioned: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   };
