0d52a0f5ce9b5314d6e0c26d36c3cb9b99d6cf47
lazy34 <maitysurajit0901@gmail.com>
2025-07-07 17:10:41 +0530

    Some changes in axios config file

--- frontend/src/lib/axios.config.js
+++ frontend/src/lib/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL_DEV,
   withCredentials: true, // This is crucial for cookies
   headers: {
     "Content-Type": "application/json",
